5a84ac66ee031bc840fc46ada3dd594f8b5cc279
emre1702 <emre1702@live.de>
2018-09-08 17:06:00 +0200

    Add helper class for MessageBox

---
+++ MessageBoxHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace MTALuaCompiler
+{
+    static class MessageBoxHelper
+    {
+
+        private static readonly BackgroundWorker worker = new BackgroundWorker();
+
+        public static void ShowError(string text)
+        {
+            worker.DoWork += ((object sender, DoWorkEventArgs e) => MessageBox.Show(text, "Error", MessageBoxButton.OK, MessageBoxImage.Error));
+            worker.RunWorkerAsync();
+        }
+
+    }
+}
