c26373640acb4f4c96dada97fb2a82738dcc93f0
emre1702 <emre1702@live.de>
2018-09-08 17:05:51 +0200

    Add helper class for console

---
+++ ConsoleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MTALuaCompiler
+{
+    static class ConsoleHelper
+    {
+        public static void WriteErrorLine(string text)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void WriteSuccessLine(string text)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
